644444fdfcdef492407ffc52ba713f7a7f8be144
Hassims <mamadouh015@gmail.com>
2026-07-04 22:51:53 +0200

    bascule

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
               fontWeight: 'bold',
             },
           }}
-        >
+        >  
           <Stack.Screen 
             name="Home" 
             component={HomeScreen} 
